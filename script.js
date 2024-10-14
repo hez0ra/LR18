3816211037affea7efd0690d1ef3215c3fd450b9
hez0ra <feleepen@gmail.com>
2024-10-14 10:00:20 +0300

    final

--- script.js
+++ script.js
@@ -14,12 +14,50 @@ button.onclick = () => {
 
     const nameReg = /^[A-Za-z]{3,30} [A-Za-z]{3,30}$/;
     const emailReg = /^[A-Za-z]{2,}@[A-Za-z]{2,}.[A-Za-z]{2,}$/;
-    const numberReg = /^+?375\s?$/
-    if(!nameReg.test(name__) || !name__.length() <= 30){
-        alert("invalid name");
+    const numberReg = /^\+?375\s?[\-\(]?\d{2}[\-\)]?\s?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}$/
+
+    const currentDate = new Date();
+    const selectedDate = new Date(birthday_);
+
+    if(!nameReg.test(name__) || name__.length > 30){
+        const err = document.getElementById('error-name');
+        err.style.display = "inline-block"
+    }
+    else{
+        const err = document.getElementById('error-name');
+        err.style.display = "none"
     }
     if(!emailReg.test(email_)){
-        alert('invalid email');
+        const err = document.getElementById('error-email');
+        err.style.display = "inline-block"
+    }
+    else{
+        const err = document.getElementById('error-email');
+        err.style.display = "none"
+    }
+    if(!numberReg.test(number_)){
+        const err = document.getElementById('error-number');
+        err.style.display = "inline-block"
+    }
+    else{
+        const err = document.getElementById('error-number');
+        err.style.display = "none"
+    }
+    if(selectedDate >= currentDate || birthday_ == ''){
+        const err = document.getElementById('error-birthday');
+        err.style.display = "inline-block"
+    }
+    else{
+        const err = document.getElementById('error-birthday');
+        err.style.display = "none"
+    }
+    if(message_.length < 10 || message_.length > 300){
+        const err = document.getElementById('error-message');
+        err.style.display = "inline-block"
+    }
+    else{
+        const err = document.getElementById('error-message');
+        err.style.display = "none"
     }
 
     
